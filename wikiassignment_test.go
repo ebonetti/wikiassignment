e76c5b1aed9922f7b56824b8b219f99c96c0a7e7
Enrico Bonetti Vieno <entanglement.ebv@gmail.com>
2019-04-14 14:54:10 +0200

    Added tests on nationalization

--- wikiassignment_test.go
+++ wikiassignment_test.go
@@ -35,3 +35,40 @@ func TestUnit(t *testing.T) {
 		t.Error("Mismatch in node count.")
 	}
 }
+
+func TestNationalizations(t *testing.T) {
+	en, err := nationalization.New("en")
+	if err != nil {
+		t.Error("Error while fetching nationalization ", err)
+	}
+	for _, lang := range nationalization.List() {
+		n, err := nationalization.New(lang)
+		if err != nil {
+			t.Error("Error while fetching nationalization ", err)
+		}
+		for i, topic := range en.Topics {
+			switch {
+			case topic.ID != n.Topics[i].ID:
+				t.Error("Topic ID not found ", topic.Title)
+			case topic.Title != n.Topics[i].Title:
+				t.Error("Topic Title not found ", topic.Title)
+			}
+		}
+	}
+}
+
+func TestNationalizationSync(t *testing.T) {
+	n := nationalization.Nationalization{
+		Language: "en",
+		Topics: []struct {
+			nationalization.Page
+			Categories []nationalization.Page `json:",omitempty"`
+		}{
+			{nationalization.Page{2147483637, "Culture and the arts"}, []nationalization.Page{{4892515, "Category:Arts"}}},
+		},
+	}
+	if nationalization.Sync(n)["en"].Topics[0].Categories[0].Title != "Category:Arts" {
+		t.Error("Category:Arts not found")
+	}
+
+}
